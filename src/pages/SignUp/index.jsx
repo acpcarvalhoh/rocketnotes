6c7d27c31491d9143624387a532c81c52688fa15
acpcarvalhoh <carvalhohuzumak@gmail.com>
2023-07-10 11:24:37 -0300

    criado tela de cadastro

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,18 +1,28 @@
-import { FiMail, FiLock } from "react-icons/fi"
+import { FiMail, FiLock, FiUser} from "react-icons/fi"
 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
+
 import { Container, Form, Background} from "./styles";
 
-export function SignIn(){
+export function SignUp(){
     return(
       <Container>
+        <Background/>
+
         <Form>
           <h1>Rocket Notes</h1>
           <p>Alpicação para salvar e gerenciar seus links úteis.</p>
 
-          <h2>Faça seu login</h2>
+          <h2>Crie Sua Conta</h2>
+
+          <Input 
+            placeholder="Nome"
+            type="text"
+            icon={FiUser}
+          />
+
 
           <Input 
             placeholder="e-mail"
@@ -26,12 +36,12 @@ export function SignIn(){
             icon={FiLock}
           />
 
-          <Button title="Entrar"/>
+          <Button title="Cadastrar"/>
 
-          <a href="#">Criar conta</a>
+          <a href="#">Voltar para o login</a>
         </Form>
 
-        <Background/>
+        
           
       </Container>
     );
